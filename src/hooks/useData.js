1eede5667daa7c5c024d2eb3bd915c7eb4d0cc82
Dmi3ii2018 <dalekseev2017@gmail.com>
2021-03-03 19:42:23 +0500

    changed http

--- src/hooks/useData.js
+++ src/hooks/useData.js
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 
-const URL = "https://zar.hosthot.ru/api/v1/pokemons/?";
+const URL = "http://zar.hosthot.ru/api/v1/pokemons/?";
 
 const usePokemonApi = (limit = 10, offset = 10) => {
   const [data, setData] = useState({ pokemons: [] });
